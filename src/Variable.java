fee0f8bc8b0affd1fddbca09a06304c82271b889
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-08-28 21:36:37 +0700

    Belajar Tentang Variabel

---
+++ src/Variable.java
@@ -0,0 +1,13 @@
+public class Variable {
+    static void main() {
+        String name;
+        name = "Dhandy Putra Fahruddin";
+        int umur;
+        umur = 20;
+
+        System.out.println("Nama : " + name + ", " + "Umur : " + umur);
+
+        var nama = "Dhandy Putra Fahruddin";
+        System.out.println(nama);
+    }
+}
