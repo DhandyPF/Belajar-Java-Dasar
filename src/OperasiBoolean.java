a2d0117af4bbb7c0de3f8a4bb180f4b645e0fa54
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-08-28 23:57:48 +0700

    Belajar Operasi Boolean di Java

---
+++ src/OperasiBoolean.java
@@ -0,0 +1,28 @@
+public class OperasiBoolean {
+    static void main() {
+        boolean value1 = true;
+        boolean value2 = false;
+
+//        System.out.println(value1 && value1);
+//        System.out.println(value1 && value2);
+//        System.out.println(value2 && value1);
+//        System.out.println(value2 && value2);
+//
+//        System.out.println(value1 || value1);
+//        System.out.println(value1 || value2);
+//        System.out.println(value2 || value1);
+//        System.out.println(value2 || value2);
+//
+//        System.out.println(!value1);
+//        System.out.println(!value2);
+
+        var absen = 80;
+        var nilaiAkhir = 70;
+
+        var lulusAbsen = absen >= 75;
+        var lulusNilaiAkhir = nilaiAkhir >= 75;
+
+        var lulus = lulusAbsen && lulusNilaiAkhir;
+        System.out.println(lulus);
+    }
+}
