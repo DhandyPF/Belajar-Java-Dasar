f5d7cc5325f925aa0792e2a05dc90ba1545fc99e
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-08-28 23:51:53 +0700

    Belajar Operasi Perbandingan di Java

---
+++ src/OperasiPerbandingan.java
@@ -0,0 +1,13 @@
+public class OperasiPerbandingan {
+    static void main() {
+        int value1 = 5;
+        int value2 = 10;
+
+        System.out.println(value1 > value2);
+        System.out.println(value1 < value2);
+        System.out.println(value1 >= value2);
+        System.out.println(value1 >= value2);
+        System.out.println(value1 == value2);
+        System.out.println(value1 != value2);
+    }
+}
