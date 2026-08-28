2447af994b1906a4988ee5d8024c56c2cfbd9466
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-08-28 11:25:35 +0700

    Belajar Tipe Data Character

---
+++ src/TipeDataChar.java
@@ -0,0 +1,16 @@
+public class TipeDataChar {
+    static void main() {
+        char d = 'D';
+        char h = 'H';
+        char a = 'A';
+        char n = 'N';
+        char y = 'Y';
+
+        System.out.print(d);
+        System.out.print(h);
+        System.out.print(a);
+        System.out.print(n);
+        System.out.print(d);
+        System.out.print(y);
+    }
+}
