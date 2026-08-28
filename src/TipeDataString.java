fbe45c04d700ff569dc6403aa1867017e739bd13
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-08-28 11:34:07 +0700

    Belajar Tipe Data String

--- src/TipeDataString.java
+++ src/TipeDataString.java
@@ -1,5 +1,9 @@
 public class TipeDataString {
     static void main() {
+        String firstName = "Dhandy Putra ";
+        String lastName = "Fahruddin ";
 
+        System.out.print(firstName);
+        System.out.print(lastName);
     }
 }
