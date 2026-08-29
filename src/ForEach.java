a38e0dba941b27bc177d62125b2a9c6bfb1610e3
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-08-29 23:18:28 +0700

    Belajar For Each di Java

---
+++ src/ForEach.java
@@ -0,0 +1,12 @@
+public class ForEach {
+    static void main() {
+        String[] names = {
+                "Dhandy", "Putra", "Fahruddin",
+                "Rezky", "Arief", "Dwi", "Syahputra"
+        };
+
+        for(var value : names) {
+            System.out.println(value);
+        }
+    }
+}
