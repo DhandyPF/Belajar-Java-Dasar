2358aa63a501b8eb96238db4f5843a2375ce3698
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-08-29 23:28:23 +0700

    Belajar Method dengan Parameter di Java

---
+++ src/MethodParameter.java
@@ -0,0 +1,10 @@
+public class MethodParameter {
+    static void main() {
+        sayHello("Dhandy", "Putra Fahruddin");
+        sayHello("Ya Ya", "Saya Setuju");
+    }
+
+    static void sayHello(String firstName, String lastName) {
+        System.out.println("Hello " + firstName + " " + lastName);
+    }
+}
