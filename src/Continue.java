ca5b43de74def0084c6cb7aea3d2462cb3a18092
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-08-29 23:14:55 +0700

    Belajar Continue di Java

--- src/Continue.java
+++ src/Continue.java
@@ -1,6 +1,6 @@
 public class Continue {
     static void main() {
-        for(var counter = 1; counter <= 100; counter++) {
+        for(var counter = 1; counter < 100; counter++) {
             if(counter % 2 == 0) {
                 continue;
             }
