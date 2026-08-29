7c50a11630cb3908a960f963ec6f6b17a5d022a2
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-08-29 23:49:21 +0700

    Belajar Method Overloading di Java

---
+++ src/MethodOverloading.java
@@ -0,0 +1,22 @@
+public class MethodOverloading {
+    static void main() {
+        sayHello();
+        sayHello("Dhandy");
+        sayHello("Dhandy", "Putra Fahruddin");
+    }
+
+    // Tanpa Parameter
+    static void sayHello() {
+        System.out.println("Hello World");
+    }
+
+    // Dengan 1 Parameter
+    static void sayHello(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    // Lebih Dari 1 Parameter
+    static void sayHello(String firstName, String lastName) {
+        System.out.println("Hello " + firstName + " " + lastName);
+    }
+}
