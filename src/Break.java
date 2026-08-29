921f04fe2237db18efb280494f6ec6a724d9691a
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-08-29 23:11:13 +0700

    Belajar Break di Java

---
+++ src/Break.java
@@ -0,0 +1,13 @@
+public class Break {
+    static void main() {
+        var counter = 1;
+        while(true) {
+            System.out.println("Perulangan " + counter);
+            counter++;
+
+            if(counter >
+                break;
+            }
+        }
+    }
+}
