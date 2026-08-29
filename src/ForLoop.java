01400b860e372887a1b1e9c8232aad1f31aa1921
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-08-29 23:00:28 +0700

    Belajar For Loop di Java

---
+++ src/ForLoop.java
@@ -0,0 +1,19 @@
+public class ForLoop {
+    static void main() {
+        // Perulangan dengan Kondisi
+        for(var counter = 1; counter <=10 ;){
+            System.out.println("Perulangan " + counter);
+            counter++;
+        }
+
+        // Perulangan dengan POST Statement
+        for(var counter = 1; counter <=10; counter++){
+            System.out.println("Perulangan " + counter);
+        }
+
+        // Perulangan Tanpa Kondisi
+//        for(;;){
+//            System.out.println("hy");
+//        }
+    }
+}
