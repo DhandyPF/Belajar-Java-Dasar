a15d6e21dbc72494063119144f7443c211dfb743
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-08-29 23:04:51 +0700

    Belajar While Loop di Java

---
+++ src/WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    static void main() {
+        var counter1 = 1;
+        while (counter1 <= 10) {
+            System.out.println("Perulangan " + counter1);
+            counter1++;
+        }
+    }
+}
