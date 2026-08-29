dcf37212507912ac437aa28df297f05b959dd254
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-08-29 23:14:35 +0700

    Belajar Continue  di Java

---
+++ src/Continue.java
@@ -0,0 +1,10 @@
+public class Continue {
+    static void main() {
+        for(var counter = 1; counter <= 100; counter++) {
+            if(counter % 2 == 0) {
+                continue;
+            }
+            System.out.println("Perulangan Ganjil " + counter);
+        }
+    }
+}
